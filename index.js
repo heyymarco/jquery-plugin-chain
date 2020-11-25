a1e8a8812aa9e55573c9acec6934bebfea044daa
heyymarco <yunuzk@gmail.com>
2020-11-25 20:40:21 +0630

    update algoritm

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var jquery_1 = __importDefault(require("jquery"));
-(function ($) {
-    $.prototype.chain = function (handler) {
-        return $(handler ? (handler.call(this) || this) : this);
-    };
-}(jquery_1.default));
+function makePlugin($) {
+    if ($ && (typeof $ == "function")) {
+        $.prototype.chain = function (handler) {
+            return $(handler ? (handler.call(this) || this) : this);
+        };
+        return $;
+    }
+    return null;
+}
+exports.default = makePlugin;
+if (jQuery)
+    makePlugin(jQuery);
