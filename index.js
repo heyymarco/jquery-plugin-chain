63b3fee9402df329019a204575a8a2f9031a0b44
heyymarco <yunuzk@gmail.com>
2020-12-07 10:51:45 +0630

    update index.js with compressed one

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var jquery_1 = __importDefault(require("jquery"));
-jquery_1.default.prototype.chain = function (handler) {
-    if (handler) {
-        var result = handler.call(this);
-        if (result) {
-            if (result === this)
-                return this;
-            return jquery_1.default(result);
-        }
-    }
-    return this;
-};
+"use strict";var __importDefault=this&&this.__importDefault||function(mod){return mod&&mod.__esModule?mod:{default:mod}};Object.defineProperty(exports,"__esModule",{value:!0});var jquery_1=__importDefault(require("jquery"));jquery_1.default.prototype.chain=function(handler){if(handler){var result=handler.call(this);if(result)return result===this?this:jquery_1.default(result)}return this};
